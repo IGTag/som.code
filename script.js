2dc9ac9d4e28b62bc28b2ea8aa40aef5a6d6b25b
Igor Leandro <igorled21@gmail.com>
2023-08-01 21:28:04 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,133 @@
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress')
+const progressContainer = document.getElementById('progress-container')
+const shuffleButton = document.getElementById('shuffle')
+
+
+const Oceans = {
+    songName: 'Oceans',
+    artist : 'Pearl Jam',
+    file : 'Oceans'
+};
+const rappSnitchKnishes = {
+    songName: 'Rapp Snitch Knishes',
+    artist : 'MF DOOM',
+    file : 'Rapp_Snitch_Knishes'
+};
+const thisCharmingMan = {
+    songName: 'This Charming Man',
+    artist : 'The Smiths',
+    file : 'This_Charming_Man'
+};
+
+let isPlaying = false;
+let isShuffled = false;
+const originalplaylist = [Oceans, rappSnitchKnishes, thisCharmingMan];
+let sortedPlaylist = [...originalplaylist];
+let index = 0;
+
+function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong(){
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+function initializeSong(){
+    cover.src = `imagens/${sortedPlaylist[index].file}.jpg`;
+    song.src =  `songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
+}
+
+function previousSong(){
+    if(index === 0){
+      index =sortedPlaylist.length - 1;  
+    }
+    else {
+        index -= 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function nextSong(){
+    if(index === sortedPlaylist.length -1){
+      index = 0;
+    }
+    else {
+      index += 1;
+   }
+    initializeSong();
+    playSong();
+
+}
+
+function updateProgressBar(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress',`${barWidth}%`);
+    
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const JumpToTime = (clickPosition/width)* song.duration;
+    song.currentTime = JumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+    const size = preShuffleArray.length;
+    let currentindex = size - 1;
+    while(currentindex > 0){
+        let randomindex = Math.floor(Math.random()*size);
+        let aux = preShuffleArray[currentindex] 
+        preShuffleArray[currentindex]=preShuffleArray[randomindex]
+        preShuffleArray[randomindex]= aux;
+        currentindex -= 1;
+    }
+}
+
+function shuffleButtonClicked() {
+    if (isShuffled === false) {
+       isShuffled = true;
+       shuffleArray(sortedPlaylist);
+       shuffleButton.classList.add('button-active');
+    } else {
+        isShuffled = false;
+        sortedPlaylist=[...originalplaylist];
+        shuffleButton.classList.remove('button-active');
+          
+    }
+}
+
+initializeSong();
+
+play.addEventListener('click',playPauseDecider);
+previous.addEventListener('click',previousSong);
+next.addEventListener('click',nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked)
+
