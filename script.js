86d0741dfc562e62129f0083be8940b028cdd53f
Igor Leandro <igorled21@gmail.com>
2023-08-08 19:19:32 -0300

    Contagem do tempo/Botão like v1

--- script.js
+++ script.js
@@ -5,77 +5,92 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
 const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime= document.getElementById('total-time');
 
 const Oceans = {
     songName: 'Oceans',
     artist : 'Pearl Jam',
-    file : 'Oceans'
+    file : 'Oceans',
+    liked: false,
 };
 
 const rappSnitchKnishes = {
     songName: 'Rapp Snitch Knishes',
     artist : 'MF DOOM',
-    file : 'Rapp_Snitch_Knishes'
+    file : 'Rapp_Snitch_Knishes',
+    liked: false,
 };
 
 const thisCharmingMan = {
     songName: 'This Charming Man',
     artist : 'The Smiths',
-    file : 'This_Charming_Man'
+    file : 'This_Charming_Man',
+    liked: false,
 };
 
 const ThemanWhoSoldTheWorld ={
     songName: 'The Man Who Sold The World',
     artist : 'David Bowie',
-    file : 'DAVIDBOWIE_THE_MAN_WHO_SOLD_THE_WORLD'
+    file : 'DAVIDBOWIE_THE_MAN_WHO_SOLD_THE_WORLD',
+    liked: false,
 };
 
 const JDM={
     songName: '151',
     artist: 'Jedi Mind Tricks',
-    file: '151'
+    file: '151',
+    liked: false,
 };
 
 const Livinprayer ={
     songName:"Livin'on A Prayer",
     artist: 'Bon Jovi',
-    file: "Livin_On_A_Prayer"
+    file: "Livin_On_A_Prayer",
+    liked: false,
 };
 
 const Pachinko ={
     songName: "DᐳEᐳAᐳTᐳHᐳMᐳEᐳTᐳAᐳL",
     artist: 'Pachinko',
     file:   'DᐳEᐳAᐳTᐳHᐳMᐳEᐳTᐳAᐳL_',
+    liked: false,
 };
 
 const shesaidiwonder = {
     songName:'She said, i wonder',
     artist:'Kudasai',
-    file: 'she_said_i_wonder'
+    file: 'she_said_i_wonder',
+    liked: false,
 }
 const seraparasempre = {
     songName:'O dia que será para sempre',
     artist:'Rodolfo Abrantes',
-    file: 'O_DIA_QUE_SERA_PARA_SEMPRE'
+    file: 'O_DIA_QUE_SERA_PARA_SEMPRE',
+    liked: false,
 }
 const whyshouldi = {
     songName:'Why Should I',
     artist:'Bob Marley',
-    file: 'WHY_SHOULD_I'
+    file: 'WHY_SHOULD_I',
+    liked: false,
 }
 const letjabepraised = {
     songName:'Let Jah Be Praised',
     artist:'Bob Marley',
-    file: 'LET_JAH_BE_PRAISED'
+    file: 'LET_JAH_BE_PRAISED',
+    liked: false,
 }
 const couldyoubeloved = {
     songName:'Could You Be Loved',
     artist:'Bob Marley',
-    file: 'COULD_YOU_BE_LOVED'
+    file: 'COULD_YOU_BE_LOVED',
+    liked: false,
 }
 
 let isPlaying = false;
@@ -112,6 +127,7 @@ function initializeSong(){
     song.src =  `songs/${sortedPlaylist[index].file}.mp3`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].artist;
+    LikeButtonRender();
 }
 
 function previousSong(){
@@ -134,12 +150,12 @@ function nextSong(){
    }
     initializeSong();
     playSong();
-
 }
 
-function updateProgressBar(){
+function updateProgress(){
     const barWidth = (song.currentTime/song.duration)*100;
     currentProgress.style.setProperty('--progress',`${barWidth}%`);
+    songTime.innerText = toHHMMSS(song.currentTime);
     
 }
 
@@ -195,13 +211,48 @@ function nextOrRepeat(){
     }
 }
 
+function toHHMMSS(originalNumber){
+    let hours = Math.floor(originalNumber / 3600);
+    let min = Math.floor((originalNumber - hours * 3600)/60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+    
+    return `${hours.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime(){
+    totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function LikeButtonRender(){
+    if (sortedPlaylist[index].liked === true){
+        likeButton.querySelector('.bi').classList.remove('bi-heart');
+        likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+        likeButton.classList.add('button-active');
+    } else{
+        likeButton.querySelector('.bi').classList.add('bi-heart');
+        likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+        likeButton.classList.remove('button-active');
+    }
+}
+
+function likeButtonClicked(){
+    if(sortedPlaylist[index].liked === false){
+       sortedPlaylist[index].liked = true;
+    } else{
+        sortedPlaylist[index].liked = false;
+    }
+    LikeButtonRender();
+}
+
 initializeSong();
 
 play.addEventListener('click',playPauseDecider);
 previous.addEventListener('click',previousSong);
 next.addEventListener('click',nextSong);
-song.addEventListener('timeupdate', updateProgressBar);
+song.addEventListener('timeupdate', updateProgress);
 song.addEventListener('ended',nextOrRepeat);
+song.addEventListener('loadedmetadata',updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click',likeButtonClicked)
